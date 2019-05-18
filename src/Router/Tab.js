9b326c07db78c871431e3cb4f99600134fa5e7ee
Kenyon Blue <bluekenyfleo@gmail.com>
2019-05-18 17:53:20 -0400

    added all tab icons with profile image

--- src/Router/Tab.js
+++ src/Router/Tab.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import {  View, Image } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import Home from './../Tabs/Home';
 import Explore from './../Tabs/Explore';
 import Likes from './../Tabs/Likes';
 import Profile from './../Tabs/Profile';
 import UpLoad from './../Tabs/UpLoad';
-import Icon from 'react-native-vector-icons/Foundation'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Feather from 'react-native-vector-icons/Feather'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import Entypo from 'react-native-vector-icons/Entypo'
@@ -16,7 +16,7 @@ const Screens = {
         screen: Home, 
         navigationOptions: () => ({
             tabBarIcon: ({ focused }) => (
-                <Icon name={'home'} size={25}  />
+                <MaterialCommunityIcons name={focused ? 'home' : 'home-outline'  } size={25}  />
         )
     }),
 },
@@ -25,7 +25,8 @@ const Screens = {
         screen: Explore, 
         navigationOptions: () => ({
             tabBarIcon: ({ focused }) => (
-                <Feather name={'search'} size={25}  />
+                focused ? <FontAwesome name={'search'} size={25}/>
+                :<Feather name={'search'} size={25}  />
         )
     }),
 },  
@@ -40,8 +41,8 @@ const Screens = {
     Likes: {
         screen: Likes, 
         navigationOptions: () => ({
-            tabBarIcon: ({tintColor, focused}) => (
-                <Entypo name={'heart-outlined'} size={25}  />
+            tabBarIcon: ({ focused}) => (
+                <Entypo name={focused ? 'heart' : 'heart-outlined'} size={25}  />
         )
     }),
 },
@@ -49,8 +50,14 @@ const Screens = {
 Profile: {
     screen: Profile, 
     navigationOptions: () => ({
-        tabBarIcon: ({tintColor, focused}) => (
-            <FontAwesome name={'user'} size={25}  />
+        tabBarIcon: ({ focused }) => (
+            <View style={{ height: 30, width: 30, borderRadius: 15, borderWidth: focused ? 1: 0, borderColor: 'grey',alignItems:'center', justifyContent: 'center'  }} >
+            <Image 
+            style={{height: 25 , width: 25, borderRadius: 25/2}}
+            source={require ('../Images/kenyon.jpg' ) }
+            />
+        </View>
+            // <FontAwesome name={ focused ? 'user': 'user-o'} size={25}  />
     )
 }),
 },
@@ -69,3 +76,4 @@ const Tab = createBottomTabNavigator(Screens, Config);
 export default Tab
 
 
+//  source={{uri: 'https://instagram.forf1-2.fna.fbcdn.net/vp/cb562ad4a8532bcf965f93e72ba078a8/5D6409BB/t51.2885-19/s320x320/32343967_183218169175351_8853912254198841344_n.jpg?_nc_ht=instagram.forf1-2.fna.fbcdn.net' }}/>
